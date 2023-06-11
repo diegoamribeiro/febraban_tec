7eb7220b523a4607f8a661c485bced5b3364a5f9
Diego <diego.ribeiro@boxdelivery.com.br>
2023-06-11 19:24:15 -0300

    Made tests on PDF Generator

--- app/src/main/java/com/cap/techsurvey/ui/OnboardFragment.kt
+++ app/src/main/java/com/cap/techsurvey/ui/OnboardFragment.kt
@@ -6,23 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import com.cap.techsurvey.R
 import com.cap.techsurvey.databinding.FragmentOnboardingBinding
 import com.cap.techsurvey.entities.Option
 import com.cap.techsurvey.entities.Question
 import com.cap.techsurvey.entities.Survey
-import com.cap.techsurvey.utils.Utils.generateQRCode
-import com.cap.techsurvey.utils.Utils.loadImage
 import com.cap.techsurvey.utils.viewBinding
-import com.itextpdf.text.Document
-import com.itextpdf.text.Paragraph
-import com.itextpdf.text.pdf.PdfWriter
 import java.io.File
-import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import com.itextpdf.kernel.font.PdfFontFactory
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.kernel.colors.ColorConstants
+import com.itextpdf.io.font.constants.StandardFonts
+import com.itextpdf.kernel.colors.DeviceRgb
 
 
 class OnboardFragment : Fragment() {
@@ -75,32 +75,52 @@ class OnboardFragment : Fragment() {
             questions = listQuestions,
             url = "https://github.com/cjnhust/ebook_collection/blob/master/Kotlin%20in%20Action.pdf"
         )
+
         val timestamp: String = SimpleDateFormat("dd_MM_yyyy_HH-mm-ss", Locale.getDefault()).format(Date())
         val fileName = "${survey.company}_$timestamp.pdf"
 
-
         val pdfFile = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName)
-        val document = Document()
-        PdfWriter.getInstance(document, FileOutputStream(pdfFile))
-        document.open()
+
+        val writer = PdfWriter(pdfFile)
+        val pdfDocument = PdfDocument(writer)
+        val document = Document(pdfDocument)
+
+        val boldFont = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD)
+        val normalFont = PdfFontFactory.createFont(StandardFonts.HELVETICA)
+
+// Adicione os dados da pesquisa ao documento
+        document.add(Paragraph("Survey ID: ${survey.id}").setFont(normalFont).setFontSize(12f))
+
+        val title = Paragraph("Name: ${survey.name}")
+            .setFont(boldFont)
+            .setFontSize(16f)
+            .setFontColor(ColorConstants.WHITE)
+            .setBackgroundColor(ColorConstants.BLUE)
+        document.add(title)
+
+        document.add(Paragraph("Email: ${survey.email}").setFont(normalFont).setFontSize(12f))
+        document.add(Paragraph("Company: ${survey.company}").setFont(normalFont).setFontSize(12f))
 
 
-        // Adiciona os dados da survey ao documento
-        document.add(Paragraph("Survey ID: ${survey.id}"))
-        document.add(Paragraph("Name: ${survey.name}"))
-        document.add(Paragraph("Email: ${survey.email}"))
-        document.add(Paragraph("Company: ${survey.company}"))
 
         for (question in survey.questions) {
-            document.add(Paragraph("Question ID: ${question.id}"))
-            document.add(Paragraph("Pergunta: ${question.text}"))
+            document.add(Paragraph("Question ID: ${question.id}").setFont(normalFont).setFontSize(12f))
+            document.add(Paragraph("Pergunta: ${question.text}").setFont(boldFont).setFontSize(14f))
             // Para cada opção na questão...
             for (option in question.options) {
-                if (option.isSelected){
-                    document.add(Paragraph("ID: ${option.id}"))
-                    document.add(Paragraph("Alternativa: ${option.text}"))
-                    document.add(Paragraph("Resposta: ${option.answer}"))
+                val paragraph: Paragraph
+                val darkGreen = DeviceRgb(0, 100, 0)
+                paragraph = if (option.isSelected) {
+                    Paragraph("Resposta: ${option.text}")
+                        .setFont(boldFont)
+                        .setFontSize(12f)
+                        .setFontColor(darkGreen) // Texto em negrito e verde escuro
+                } else {
+                    Paragraph("Resposta: ${option.text}")
+                        .setFont(normalFont)
+                        .setFontSize(12f)
                 }
+                document.add(paragraph)
             }
         }
 
@@ -110,15 +130,6 @@ class OnboardFragment : Fragment() {
         //generateQRCode(url, binding.ivCode)
         return binding.root
     }
-
-
-
-
-
-
-
-
-
 
 
 }
